ed454d1d8a0d4d56803210f1a9d54c66cee7987e
Chris Oates <chrispoates@gmail.com>
2023-09-15 15:00:27 -0400

    ZLayerExperiments throws error if num isn't in expectd range and I added ZStreamExperiments (empty file).

--- src/main/scala/ZLayerExperiments.scala
+++ src/main/scala/ZLayerExperiments.scala
@@ -2,24 +2,15 @@ import zio._
 
 object ZLayerExperiments {
     trait NumberGenerator {
-        def get: Task[Option[Int]]
+        def get: Task[Int]
 
         def put(n: Int): Task[Unit]
     }
 
-    class NumberGeneratorLive extends NumberGenerator {
-        def get: Task[Option[Int]] = for {
-            _ <- Console.printLine("Hey! I'm a program. Give me a number!")
-            num <- Console.readLine
-            doubled <- ZIO.attempt(num.toInt * 2)
-            _ <- Console.printLine(s"The number doubled is: $doubled")
-        } yield Some(doubled)
+    object NumberGenerator {
+        def get: ZIO[NumberGenerator, Throwable, Int] = ZIO.serviceWithZIO[NumberGenerator](_.get)
 
-        def put(n: Int): Task[Unit] = Console.printLine(s"Putting $n into the queue")
-    }
-
-    object NumberGeneratorLive {
-        val layer: ZLayer[Any, Nothing, NumberGeneratorLive] = ZLayer.succeed(new NumberGeneratorLive)
+        def put(n: Int): ZIO[NumberGenerator, Throwable, Unit] = ZIO.serviceWithZIO[NumberGenerator](_.put(n))
     }
 
     trait Database {
@@ -28,10 +19,35 @@ object ZLayerExperiments {
         def getUserName(id: Int): IO[Throwable, String]
     }
 
+    object Database {
+        def dbName(): ZIO[Database, Nothing, String] = ZIO.serviceWith[Database](_.dbName())
+
+        def getUserName(id: Int): ZIO[Database, Throwable, String] = ZIO.serviceWithZIO[Database](_.getUserName(id))
+    }
+
+
+    class NumberGeneratorLive extends NumberGenerator {
+        def get: Task[Int] = for {
+            _ <- Console.printLine("Give me a number!! (must be less than 100)")
+            input <- Console.readLine
+            num <- ZIO.attempt(input.toInt)
+        } yield num
+
+        def put(n: Int): Task[Unit] = Console.printLine(s"Putting $n into the queue")
+    }
+
+    object NumberGeneratorLive {
+        val layer: ZLayer[Any, Nothing, NumberGeneratorLive] = ZLayer.succeed(new NumberGeneratorLive)
+    }
+
     class DatabaseLive extends Database {
         def dbName(): String = "mockdb"
 
-        def getUserName(id: Int): IO[Throwable, String] = ZIO.succeed(s"user$id")
+        def getUserName(id: Int): IO[Throwable, String] = if (id < 100) {
+            ZIO.succeed(s"user$id")
+        } else {
+            ZIO.fail(new Exception(s"No users with id=$id. Id must be <100"))
+        }
     }
 
     object DatabaseLive {
@@ -40,10 +56,8 @@ object ZLayerExperiments {
 
     val theProgram: ZIO[Database with NumberGenerator, Throwable, Unit] = {
         for {
-            numQ <- ZIO.service[NumberGenerator]
-            num <- numQ.get.map(_.getOrElse(0))
-            db <- ZIO.service[Database]
-            username <- db.getUserName(num)
+            num <- NumberGenerator.get
+            username <- Database.getUserName(num) // TODO make this an option and handle as an erro
             _ <- Console.printLine(s"The username fetched from the database is: $username")
         } yield ()
     }
