62968a49b0f20669522a9ab998495531f4316a18
Chris Oates <chrispoates@gmail.com>
2025-04-30 13:55:24 -0400

    I dunno, something I backed up before reimaging

--- src/main/scala/Sandbox.scala
+++ src/main/scala/Sandbox.scala
@@ -1,32 +1,68 @@
 import java.io.IOException
 import zio._
 import zio.stream.{ZSink, ZStream}
+import zio.Schedule.WithState
+
+trait LoadProgress
+
+trait NotificationBucketEnqueuer {
+    type NotificationBucket = Int
+    protected val loadOperation: Task[(NotificationBucket, LoadProgress)]
+    protected val isFinishedLoading: LoadProgress => Boolean
+
+    def enqueueStream(): ZIO[Any, Throwable, Unit] = {
+        ZStream.fromZIO(loadOperation)
+            .repeat(Schedule.recurUntil { case (_: NotificationBucket, progress: LoadProgress) =>
+                isFinishedLoading(progress)
+            case _ => ZIO.fail(new IOException("Unexpected LoadProgress type"))
+            })
+            .map { case (bucket, _) => bucket }
+            .tap { bucket =>
+                Console.printLine(s"tapping stream: $bucket")
+            }
+            .run(ZSink.drain)
+    }
+}
+
+case class RowsLoaded(rows: Int) extends LoadProgress
+
+class PassUpdateASBEnqueuer extends NotificationBucketEnqueuer {
+    var counter = 0
+
+    def dbOperation(): (NotificationBucket, LoadProgress) = {
+        counter += 1
+        println(s"DB operation $counter")
+        val rowsLoaded = if (counter < 5) 1 else 0
+        (counter, RowsLoaded(rowsLoaded))
+    }
+
+    protected val loadOperation: Task[(NotificationBucket, LoadProgress)] = ZIO.attempt(dbOperation())
+    protected val isFinishedLoading: LoadProgress => Boolean = {
+        case RowsLoaded(rows) => rows == 0
+    }
+}
 
 object Sandbox {
 
     var counter = 0
     var isRunning = true
 
-    def dbOperation(): Unit = {
+    def dbOperation(): Int = {
         counter += 1
-        if (counter > 3) isRunning = false
         println(s"DB operation $counter")
+        counter
     }
 
+    val dbOperationZIO: ZIO[Any, Throwable, Int] = ZIO.attempt(dbOperation())
 
-
-    val dbOperationZIO: ZIO[Any, Throwable, Unit] = ZIO.attempt(dbOperation())
-
-    val dbOperationStream: ZStream[Any, Throwable, Unit] =
+    val dbOperationStream: ZStream[Any, Throwable, Int] =
         ZStream.fromZIO(dbOperationZIO)
+            .repeat(Schedule.recurUntil(_ => counter >= 5 && isRunning))
 
-    def runStream(z: ZStream[Any, Throwable, Unit]): ZIO[Any, Throwable, Unit] = {
-        z
-            .repeat(Schedule.forever)
-            .tap { _ =>
-                Console.printLine(s"tapping repeated stream: $counter")
+    def runStream(z: ZStream[Any, Throwable, Int]): ZIO[Any, Throwable, Unit] = {
+        z.tap { _ =>
+                Console.printLine(s"tapping stream: $counter")
             }
-            .takeWhile(_ => isRunning)
             .run(ZSink.drain)
     }
 
@@ -35,24 +71,14 @@ object Sandbox {
         isRunning = true
     }
 
+    val enqueuer = new PassUpdateASBEnqueuer()
+
     val theProgram: ZIO[Any, Throwable, Unit] = for {
         _ <- Console.printLine("The Stream:")
-        _ <- ZStream(dbOperation _) // Method 1: Use a function reference
-            .map(foo => foo())
-            .tap(_ => Console.printLine(s"tap: $counter"))
-            .repeat(Schedule.forever)
-            .takeWhile(_ => counter < 5 && isRunning)
-            .tap(_ => Console.printLine(s"tap: $counter"))
-            .run(ZSink.drain)
         _ = resetCounters()
-        _ <- ZStream.fromZIO(dbOperationZIO) // Method 2: Use fromZIO
-            .tap(_ => Console.printLine(s"tap: $counter"))
-            .repeat(Schedule.forever)
-            .takeWhile(_ => counter < 5 && isRunning)
-            .tap(_ => Console.printLine(s"tap: $counter"))
-            .run(ZSink.drain)
-        _ = resetCounters()
-        _ <- runStream(dbOperationStream) // ...
+        _ <- runStream(dbOperationStream)
+        _ <- Console.printLine("The Stream has finished")
+        _ <- enqueuer.enqueueStream()
     } yield ()
 
     def run(): Unit = Main.run(
